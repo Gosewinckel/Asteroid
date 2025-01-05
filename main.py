5778d45f6f3df6c9758054b1739e0bb2567b8869
Gosewinckel <gosewinckel.lewis@gmail.com>
2025-01-05 23:07:19 +1100

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+from constants import *
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+while True:
+    screen.fill("black")
+    pygame.display.flip()
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
