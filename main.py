3eb9ad6f7ded1144c7d57e6c35ec6bbbb1d8dde6
Gosewinckel <gosewinckel.lewis@gmail.com>
2025-01-06 10:47:14 +1100

    triangle appears on screen

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *
+import player
+import circleshape
+
 
 
 
@@ -12,17 +15,25 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    frames = pygame.time.Clock
+    frames = pygame.time.Clock()
     dt = 0
-
+    
+    spaceship = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
+        screen.fill("black")
+        spaceship.draw(screen)
+        
+        
+        #end of iteration
+        dt = frames.tick(60)/1000
+        pygame.display.flip()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-    screen.fill("black")
-    pygame.display.flip()
-    dt = frames.tick(60)/1000
+    
+
 
 
 
